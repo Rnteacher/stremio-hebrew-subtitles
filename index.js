c689a1a47fd04458b31651c35e96bd88dfec96de
Ronen <rnteacher@gmail.com>
2025-05-02 22:37:27 +0300

    עדכון כולל לקבצים

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const port = 10000;
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path'); // Import path module
